e7c8b6c9c39402461b88c58dfcd98ee7de8e5fbe
EricElmoznino <eric.elmoznino@gmail.com>
2019-12-17 17:12:26 -0500

    Using simple nearest upsampling instead of conv2dtranspose, since original size is just (1,1)

--- vae/models/core.py
+++ vae/models/core.py
@@ -4,36 +4,34 @@ from .conv_lstm import Conv2dLSTMCell
 
 
 class InferenceCore(nn.Module):
-    def __init__(self, c_dim, v_dim, r_dim, h_dim):
+    def __init__(self, c_dim, r_dim, h_dim):
         super().__init__()
         self.downsample_x = nn.Conv2d(c_dim, c_dim, kernel_size=4, stride=4, padding=0, bias=False)
         self.upsample = nn.Upsample(size=(16, 16))
         self.downsample_u = nn.Conv2d(h_dim, h_dim, kernel_size=4, stride=4, padding=0, bias=False)
-        self.core = Conv2dLSTMCell(c_dim + v_dim + r_dim + 2 * h_dim, h_dim, kernel_size=5, stride=1, padding=2)
+        self.core = Conv2dLSTMCell(c_dim + r_dim + 2 * h_dim, h_dim, kernel_size=5, stride=1, padding=2)
 
-    def forward(self, x, v, r, c_e, h_e, h_g, u):
+    def forward(self, x, r, c_e, h_e, h_g, u):
         x = self.downsample_x(x)
-        v = self.upsample(v.view(v.size(0), -1, 1, 1))
         if r.size(2) != h_e.size(2):
             r = self.upsample(r)
         u = self.downsample_u(u)
-        c_e, h_e = self.core(torch.cat((x, v, r, h_g, u), dim=1), (c_e, h_e))
+        c_e, h_e = self.core(torch.cat((x, r, h_g, u), dim=1), (c_e, h_e))
 
         return c_e, h_e
 
 
 class GenerationCore(nn.Module):
-    def __init__(self, v_dim, r_dim, z_dim, h_dim):
+    def __init__(self, r_dim, z_dim, h_dim):
         super().__init__()
         self.upsample = nn.Upsample(size=(16, 16))
-        self.core = Conv2dLSTMCell(v_dim + r_dim + z_dim, h_dim, kernel_size=5, stride=1, padding=2)
+        self.core = Conv2dLSTMCell(r_dim + z_dim, h_dim, kernel_size=5, stride=1, padding=2)
         self.upsample_h = nn.ConvTranspose2d(h_dim, h_dim, kernel_size=4, stride=4, padding=0, bias=False)
 
-    def forward(self, v, r, c_g, h_g, u, z):
-        v = self.upsample(v.view(v.size(0), -1, 1, 1))
+    def forward(self, r, c_g, h_g, u, z):
         if r.size(2) != h_g.size(2):
             r = self.upsample(r)
-        c_g, h_g = self.core(torch.cat((v, r, z), dim=1), (c_g, h_g))
+        c_g, h_g = self.core(torch.cat((r, z), dim=1), (c_g, h_g))
         u = self.upsample_h(h_g) + u
 
         return c_g, h_g, u
--- vae/models/generator.py
+++ vae/models/generator.py
@@ -8,12 +8,11 @@ using convolutional and recurrent networks.
 Additionally, a representation vector is shared between
 the networks.
 """
-SCALE = 4  # Scale of image generation process
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributions import Normal, kl_divergence
+from .core import InferenceCore, GenerationCore
 
 
 class GeneratorNetwork(nn.Module):
@@ -27,179 +26,115 @@ class GeneratorNetwork(nn.Module):
     :param z_dim: latent channels
     :param h_dim: hidden channels in LSTM
     :param l: number of density refinements
-    :param share: whether to share cores across refinements
+    :param shared_core: whether to share cores across refinements
     """
 
-    def __init__(self, c_dim, r_dim, z_dim=64, h_dim=128, l=8, share=True):
+    def __init__(self, c_dim, r_dim, z_dim=3, h_dim=128, l=8, shared_core=True):
         super().__init__()
-        self.l = l
+
         self.z_dim = z_dim
         self.h_dim = h_dim
-        self.share = share
+        self.l = l
 
-        # Core computational units
-        kwargs = dict(kernel_size=5, stride=1, padding=2)
-        inference_args = dict(in_channels=r_dim + c_dim + h_dim, out_channels=h_dim, **kwargs)
-        generator_args = dict(in_channels=r_dim + z_dim, out_channels=h_dim, **kwargs)
-        if self.share:
-            self.inference_core = Conv2dLSTMCell(**inference_args)
-            self.generator_core = Conv2dLSTMCell(**generator_args)
+        # Generation network
+        self.shared_core = shared_core
+        if shared_core:
+            self.inference_core = InferenceCore(c_dim, r_dim, h_dim)
+            self.generation_core = GenerationCore(r_dim, z_dim, h_dim)
         else:
-            self.inference_core = nn.ModuleList([Conv2dLSTMCell(**inference_args) for _ in range(l)])
-            self.generator_core = nn.ModuleList([Conv2dLSTMCell(**generator_args) for _ in range(l)])
+            self.inference_core = nn.ModuleList([InferenceCore(c_dim, r_dim, h_dim) for _ in range(l)])
+            self.generation_core = nn.ModuleList([GenerationCore(r_dim, z_dim, h_dim) for _ in range(l)])
 
-        # Inference, prior
-        self.posterior_density = nn.Conv2d(h_dim, 2 * z_dim, **kwargs)
-        self.prior_density = nn.Conv2d(h_dim, 2 * z_dim, **kwargs)
-
-        # Generative density
-        self.observation_density = nn.Conv2d(h_dim, c_dim, kernel_size=1, stride=1, padding=0)
-
-        # Up/down-sampling primitives
-        self.upsample = nn.ConvTranspose2d(h_dim, h_dim, kernel_size=SCALE, stride=SCALE, padding=0, bias=False)
-        self.downsample = nn.Conv2d(c_dim, c_dim, kernel_size=SCALE, stride=SCALE, padding=0, bias=False)
+        self.eta_pi = nn.Conv2d(h_dim, 2*z_dim, kernel_size=5, stride=1, padding=2)
+        self.eta_g = nn.Conv2d(h_dim, c_dim, kernel_size=1, stride=1, padding=0)
+        self.eta_e = nn.Conv2d(h_dim, 2*z_dim, kernel_size=5, stride=1, padding=2)
 
     def forward(self, x, r):
         """
         Attempt to reconstruct x with corresponding
         context representation r.
 
-        :param x: image to send through
+        :param x: image to send reconstruct
         :param r: representation for image
         :return reconstruction of x and kl-divergence
         """
-        batch_size, _, h, w = x.shape
+        batch_size = x.size(0)
+
+        # Generator initial state
+        c_g = x.new_zeros((batch_size, self.h_dim, 16, 16))
+        h_g = x.new_zeros((batch_size, self.h_dim, 16, 16))
+        u = x.new_zeros((batch_size, self.h_dim, 64, 64))
+
+        # Inference initial state
+        c_e = x.new_zeros((batch_size, self.h_dim, 16, 16))
+        h_e = x.new_zeros((batch_size, self.h_dim, 16, 16))
+
         kl = 0
-
-        # Downsample x, upsample r
-        x = self.downsample(x)
-        if r.size(2) != h // SCALE:
-            r = r.repeat(1, 1, h // SCALE, w // SCALE)
-
-        # Reset hidden and cell state
-        hidden_i = x.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-        cell_i = x.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-
-        hidden_g = x.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-        cell_g = x.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-
-        # Canvas for updating
-        u = x.new_zeros((batch_size, self.h_dim, h, w))
-
         for l in range(self.l):
-            # Prior factor (eta π network)
-            p_mu, p_std = torch.chunk(self.prior_density(hidden_g), 2, dim=1)
-            prior_distribution = Normal(p_mu, F.softplus(p_std))
+            # Prior factor
+            mu_pi, logvar_pi = torch.split(self.eta_pi(h_g), self.z_dim, dim=1)
+            std_pi = torch.exp(0.5*logvar_pi)
+            pi = Normal(mu_pi, std_pi)
 
             # Inference state update
-            inference = self.inference_core if self.share else self.inference_core[l]
-            hidden_i, cell_i = inference(torch.cat([hidden_g, x, r], dim=1), [hidden_i, cell_i])
+            if self.shared_core:
+                c_e, h_e = self.inference_core(x, r, c_e, h_e, h_g, u)
+            else:
+                c_e, h_e = self.inference_core[l](x, r, c_e, h_e, h_g, u)
 
-            # Posterior factor (eta e network)
-            q_mu, q_std = torch.chunk(self.posterior_density(hidden_i), 2, dim=1)
-            posterior_distribution = Normal(q_mu, F.softplus(q_std))
+            # Posterior factor
+            mu_q, logvar_q = torch.split(self.eta_e(h_e), self.z_dim, dim=1)
+            std_q = torch.exp(0.5*logvar_q)
+            q = Normal(mu_q, std_q)
 
             # Posterior sample
-            z = posterior_distribution.rsample()
+            z = q.rsample()
 
             # Generator state update
-            generator = self.generator_core if self.share else self.generator_core[l]
-            hidden_g, cell_g = generator(torch.cat([z, r], dim=1), [hidden_g, cell_g])
+            if self.shared_core:
+                c_g, h_g, u = self.generation_core(r, c_g, h_g, u, z)
+            else:
+                c_g, h_g, u = self.generation_core[l](r, c_g, h_g, u, z)
 
-            # Calculate u
-            u = self.upsample(hidden_g) + u
+            # KL update
+            kl += kl_divergence(q, pi)
 
-            # Calculate KL-divergence
-            kl += kl_divergence(posterior_distribution, prior_distribution)
+        x_mu = self.eta_g(u)
+        x_mu = torch.sigmoid(x_mu)
 
-        x_mu = self.observation_density(u)
+        return x_mu, kl
 
-        return torch.sigmoid(x_mu), kl
-
-    def sample(self, x_shape, r):
+    def sample(self, r):
         """
         Sample from the prior distribution to generate
         a new image given a representation
 
-        :param x_shape: (height, width) of image
         :param r: representation (context)
         """
-        h, w = x_shape
         batch_size = r.size(0)
 
-        # Increase dimensions
-        if r.size(2) != h // SCALE:
-            r = r.repeat(1, 1, h // SCALE, w // SCALE)
+        # Initial state
+        c_g = r.new_zeros((batch_size, self.h_dim, 16, 16))
+        h_g = r.new_zeros((batch_size, self.h_dim, 16, 16))
+        u = r.new_zeros((batch_size, self.h_dim, 64, 64))
 
-        # Reset hidden and cell state for generator
-        hidden_g = r.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-        cell_g = r.new_zeros((batch_size, self.h_dim, h // SCALE, w // SCALE))
-
-        u = r.new_zeros((batch_size, self.h_dim, h, w))
-
-        for _ in range(self.l):
-            p_mu, p_log_std = torch.chunk(self.prior_density(hidden_g), 2, dim=1)
-            prior_distribution = Normal(p_mu, F.softplus(p_log_std))
+        for l in range(self.l):
+            # Prior factor
+            mu_pi, logvar_pi = torch.split(self.eta_pi(h_g), self.z_dim, dim=1)
+            std_pi = torch.exp(0.5*logvar_pi)
+            pi = Normal(mu_pi, std_pi)
 
             # Prior sample
-            z = prior_distribution.sample()
+            z = pi.sample()
 
-            # Calculate u
-            hidden_g, cell_g = self.generator_core(torch.cat([z, r], dim=1), [hidden_g, cell_g])
-            u = self.upsample(hidden_g) + u
+            # State update
+            if self.shared_core:
+                c_g, h_g, u = self.generation_core(r, c_g, h_g, u, z)
+            else:
+                c_g, h_g, u = self.generation_core[l](r, c_g, h_g, u, z)
 
-        x_mu = self.observation_density(u)
+        # Image sample
+        x_mu = self.eta_g(u)
+        x_mu = torch.sigmoid(x_mu)
 
-        return torch.sigmoid(x_mu)
-
-
-class Conv2dLSTMCell(nn.Module):
-    """
-    2d convolutional long short-term memory (LSTM) cell.
-    Functionally equivalent to nn.LSTMCell with the
-    difference being that nn.Kinear layers are replaced
-    by nn.Conv2D layers.
-
-    :param in_channels: number of input channels
-    :param out_channels: number of output channels
-    :param kernel_size: size of image kernel
-    :param stride: length of kernel stride
-    :param padding: number of pixels to pad with
-    """
-
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-
-        kwargs = dict(kernel_size=kernel_size, stride=stride, padding=padding)
-
-        self.forget = nn.Conv2d(in_channels, out_channels, **kwargs)
-        self.input = nn.Conv2d(in_channels, out_channels, **kwargs)
-        self.output = nn.Conv2d(in_channels, out_channels, **kwargs)
-        self.state = nn.Conv2d(in_channels, out_channels, **kwargs)
-
-        self.transform = nn.Conv2d(out_channels, in_channels, **kwargs)
-
-    def forward(self, input, states):
-        """
-        Send input through the cell.
-
-        :param input: input to send through
-        :param states: (hidden, cell) pair of internal state
-        :return new (hidden, cell) pair
-        """
-        (hidden, cell) = states
-
-        input = input + self.transform(hidden)
-
-        forget_gate = torch.sigmoid(self.forget(input))
-        input_gate = torch.sigmoid(self.input(input))
-        output_gate = torch.sigmoid(self.output(input))
-        state_gate = torch.tanh(self.state(input))
-
-        # Update internal cell state
-        cell = forget_gate * cell + input_gate * state_gate
-        hidden = output_gate * torch.tanh(cell)
-
-        return hidden, cell
+        return x_mu
