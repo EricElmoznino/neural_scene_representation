76fa97c2f8f5507a062dd5471cf369081f3a1ece
EricElmoznino <eric.elmoznino@gmail.com>
2020-01-19 16:26:31 -0500

    Put normalization function in the dataset rendering script instead

--- gqn/datasets/XYRHDataset.py
+++ gqn/datasets/XYRHDataset.py
@@ -12,25 +12,3 @@ class XYRHDataset(FoldersDataset):
         v_hat = [l, torch.cos(y), torch.sin(y), torch.cos(p), torch.sin(p)]
         v_hat = torch.cat(v_hat, dim=-1)
         return v_hat
-
-
-def normalize_locations(data_dir):
-    """
-    Utility function to run on the dataset before any training
-    in order to normalize the viewpoint locations to range (-1, 1)
-    :param data_dir:
-    """
-    scenes = os.listdir(data_dir)
-    scenes = [s for s in scenes if s != '.DS_Store']
-    scenes = [os.path.join(data_dir, s) for s in scenes]
-    viewpoints = [os.path.join(s, 'viewpoints.npy') for s in scenes]
-    max_range = 0
-    for path in viewpoints:
-        viewpoint = np.load(path)
-        r = np.fabs(viewpoint[:, 0:2]).max()
-        if r > max_range:
-            max_range = r
-    for path in viewpoints:
-        viewpoint = np.load(path)
-        viewpoint[:, 0:2] = viewpoint[:, 0:2] / max_range
-        np.save(path, viewpoint)
